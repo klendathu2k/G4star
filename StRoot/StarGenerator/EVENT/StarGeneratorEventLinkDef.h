6a7fbf60fc3b885efe14517f32059b3f0ab7cd13
klendathu2k <jwebb@bnl.gov>
2020-05-07 08:40:29 -0400

    StarGenerator

---
+++ StRoot/StarGenerator/EVENT/StarGeneratorEventLinkDef.h
@@ -0,0 +1,12 @@
+/* This was generated for version 'DEV' */
+#pragma link off all globals;
+#pragma link off all classes;
+#pragma link off all functions;
+#ifdef __CINT__
+#pragma link C++ class StarGenAAEvent+;
+#pragma link C++ class StarGenEPEvent+;
+#pragma link C++ class StarGenEvent+;
+#pragma link C++ class StarGenPPEvent+;
+#pragma link C++ class StarGenParticle+;
+#pragma link C++ class StarGenStats+;
+#endif
