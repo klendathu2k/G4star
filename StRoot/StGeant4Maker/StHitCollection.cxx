db57603893f6cd5f8f93ec37857c420da1de5811
klendathu2k <jwebb@bnl.gov>
2020-05-08 16:33:04 -0400

    Define Tracker and Calorimeter hit "collections", which are responsible for creation, storage *and* filling of the hit objects.

---
+++ StRoot/StGeant4Maker/StHitCollection.cxx
@@ -0,0 +1,209 @@
+#include <StHitCollection.h>
+
+#include <TVirtualMC.h>
+#include <TGeoManager.h>
+#include <TGeoNavigator.h>
+
+#include <StSensitiveDetector.h>
+#include <StMCParticleStack.h>
+#include <StMessMgr.h>
+
+//_____________________________________________________________________________________________
+StHitCollection::StHitCollection( const char* name, const char* title ) : TNamed(name,title){ }
+//_____________________________________________________________________________________________
+StTrackerHitCollection::StTrackerHitCollection( const char* name, const char* title ) : StHitCollection(name,title), mHits() { }
+//_____________________________________________________________________________________________
+StCalorimeterHitCollection::StCalorimeterHitCollection( const char* name, const char* title ) : StHitCollection(name,title), mHits(),mBirk{1.0,0.0130,9.6E-6},mEsum(0) { }
+//_____________________________________________________________________________________________
+
+
+//_____________________________________________________________________________________________
+void StTrackerHitCollection::Initialize() {
+
+}
+//_____________________________________________________________________________________________
+void StTrackerHitCollection::ProcessHits() {
+
+  TGeoNavigator* navigator = gGeoManager->GetCurrentNavigator();
+  TVirtualMC*    mc = TVirtualMC::GetMC();
+
+  // Is this a charged particle?  If not, skip it...
+  if ( 0 == mc->TrackCharge() ) return;
+
+
+  // Energy deposited in this tracking step
+  double Edep = mc->Edep();
+
+  // Position and momentum of the hit 
+  double x, y, z, px, py, pz, etot;
+  mc->TrackPosition( x, y, z );
+  mc->TrackMomentum( px, py, pz, etot ); 
+
+  StMCParticleStack* stack = (StMCParticleStack *)mc->GetStack();
+  
+  // Get list of tracks from particle stack
+  std::vector<StarMCParticle*>& truthTable = stack->GetParticleTable();
+
+  // This should be the current particle truth 
+  StarMCParticle* truth = truthTable.back();
+  truth->addHit(); 
+
+  bool isNewTrack      = mc->IsNewTrack();
+  bool isTrackEntering = mc->IsTrackEntering();
+  bool isTrackExiting  = mc->IsTrackExiting();
+  bool isTrackInside   = mc->IsTrackInside();
+  bool isTrackOut      = mc->IsTrackOut();
+  bool isTrackStop     = mc->IsTrackStop();
+
+  TrackerHit* hit = 0;
+  
+  // Track has entered this volume, create a new hit
+  if ( isTrackEntering || isNewTrack ) {
+
+    // Make sure that the level is not too deep
+    if ( navigator->GetLevel() > int(DetectorHit::maxdepth) ) {
+      LOG_INFO << "Cannot score hits with depth " <<   navigator->GetLevel() << endm;
+      return; 
+    }
+    mHits.push_back( hit = new TrackerHit );
+
+    hit->path = mc->CurrentVolPath(); 
+
+    // Assign the hit a unqiue ID (index + 1)
+    hit->id = mHits.size();
+
+    // Assign the hit the ID truth of the current track (index + 1)
+    hit->idtruth = truthTable.size();
+
+    // Score entrance momentum and 
+    mc->TrackMomentum( hit->momentum_in[0], hit->momentum_in[1],  hit->momentum_in[2],  hit->momentum_in[3] ); 
+    mc->TrackPosition( hit->position_in[0], hit->position_in[1],  hit->position_in[2] );
+    hit->position_in[3] = mc->TrackTime();
+ 
+  } 
+  
+  if ( mHits.size() == 0 ) {
+    LOG_INFO << "No available hits" << endm;
+    return; 
+  }
+
+  // Update the final step in this hit
+  hit = mHits.back();
+  
+  hit -> nsteps += 1; 
+
+  // For all other tracking states, update the exit momentum and position 
+  mc->TrackMomentum( hit->momentum_out[0], hit->momentum_out[1],  hit->momentum_out[2],  hit->momentum_out[3] ); 
+  mc->TrackPosition( hit->position_out[0], hit->position_out[1],  hit->position_out[2] );
+  hit->position_out[3] = mc->TrackTime();
+
+  // Increment the energy loss and step sums
+  hit -> de += mc->Edep();
+  hit -> ds += mc->TrackStep();
+
+  // Total length to this point
+  hit -> length = mc->TrackLength();
+
+  double mass = mc->TrackMass();
+  double Etot = mc->Etot(); // total energy
+
+}
+//_____________________________________________________________________________________________
+void StTrackerHitCollection::EndOfEvent() {
+
+}
+//_____________________________________________________________________________________________
+
+
+//_____________________________________________________________________________________________
+void StCalorimeterHitCollection::Initialize() {
+
+}
+//_____________________________________________________________________________________________
+void StCalorimeterHitCollection::ProcessHits() {
+
+  TGeoNavigator* navigator = gGeoManager->GetCurrentNavigator();
+  TVirtualMC*    mc = TVirtualMC::GetMC();
+
+  // Is this a charged particle?  If not, skip it...
+  if ( 0 == mc->TrackCharge() ) return;
+
+  // Energy deposited in this tracking step
+  double Edep = mc->Edep();
+
+  // Correct for Birk's law
+  //  Edep = Edep * mBirk[0] / ( 1.0 + mBirk[1]*Edep + mBirk[2]*Edep*Edep );
+
+  // Position and momentum of the hit 
+  double x, y, z, px, py, pz, etot;
+  mc->TrackPosition( x, y, z );
+  mc->TrackMomentum( px, py, pz, etot ); 
+
+  StMCParticleStack* stack = (StMCParticleStack *)mc->GetStack();
+  
+  // Get list of tracks from particle stack
+  std::vector<StarMCParticle*>& truthTable = stack->GetParticleTable();
+
+  // This should be the current particle truth 
+  StarMCParticle* truth = truthTable.back();
+  truth->addHit(); 
+
+  bool isNewTrack      = mc->IsNewTrack();
+  bool isTrackEntering = mc->IsTrackEntering();
+  bool isTrackExiting  = mc->IsTrackExiting();
+  bool isTrackInside   = mc->IsTrackInside();
+  bool isTrackOut      = mc->IsTrackOut();
+  bool isTrackStop     = mc->IsTrackStop();
+
+  CalorimeterHit* hit = 0;
+  
+  // Track has entered this volume, create a new hit
+  if ( isTrackEntering || isNewTrack ) {
+
+    // Zero out the energy sum
+    mEsum = 0;
+
+    // Make sure that the level is not too deep
+    if ( navigator->GetLevel() > int(DetectorHit::maxdepth) ) {
+      LOG_INFO << "Cannot score hits with depth " <<   navigator->GetLevel() << endm;
+      return; 
+    }
+
+    mHits.push_back( hit = new CalorimeterHit );
+
+    hit->path = mc->CurrentVolPath(); 
+
+    // Assign the hit a unqiue ID (index + 1)
+    hit->id = mHits.size();
+
+    // Assign the hit the ID truth of the current track (index + 1)
+    hit->idtruth = truthTable.size();
+
+    // Score entrance
+    mc->TrackPosition( hit->position_in[0], hit->position_in[1],  hit->position_in[2] );
+    hit->position_in[3] = mc->TrackTime();
+ 
+  } 
+  
+  if ( mHits.size() == 0 ) {
+    LOG_INFO << "No available hits" << endm;
+    return; 
+  }
+
+  // Update the final step in this hit
+  hit = mHits.back();
+  
+  hit -> nsteps += 1; 
+
+  // Add current energy deposit to energy sum
+  mEsum += Edep;
+
+  // Hit energy will be the total energy deposition corrected by Birk's law
+  hit -> de =   mEsum * mBirk[0] / ( 1.0 + mBirk[1]*mEsum + mBirk[2]*mEsum*mEsum );
+
+}
+//_____________________________________________________________________________________________
+void StCalorimeterHitCollection::EndOfEvent() {
+
+}
+//_____________________________________________________________________________________________
---
+++ StRoot/StGeant4Maker/StHitCollection.h
@@ -0,0 +1,70 @@
+#ifndef __StHitCollection_h__
+#define __StHitCollection_h__
+
+class DetectorHit;
+class TrackerHit;
+class CalorimeterHit;
+
+#include <TNamed.h>
+#include <TVector.h>
+
+class StHitCollection : public TNamed {
+
+public:
+
+  StHitCollection( const char* name, const char* title );
+ ~StHitCollection(){ /* nada */ };
+
+  virtual void Initialize()  = 0;
+  virtual void ProcessHits() = 0;
+  virtual void EndOfEvent()  = 0;
+
+private:
+protected:
+  
+  ClassDef(StHitCollection,0);
+
+};
+
+class StTrackerHitCollection : public StHitCollection {
+
+public:
+  StTrackerHitCollection( const char* name, const char* title );
+ ~StTrackerHitCollection() { /* nada */ };
+
+  virtual void Initialize();
+  virtual void ProcessHits();
+  virtual void EndOfEvent();
+
+private:
+protected:
+
+  std::vector<TrackerHit*> mHits;
+
+  ClassDef(StTrackerHitCollection,0);
+
+};
+
+ 
+class StCalorimeterHitCollection : public StHitCollection {
+
+public:
+  StCalorimeterHitCollection( const char* name, const char* title );
+ ~StCalorimeterHitCollection() { /* nada */ };
+
+  virtual void Initialize();
+  virtual void ProcessHits();
+  virtual void EndOfEvent();
+
+private:
+protected:
+
+  std::vector<CalorimeterHit*> mHits;
+  double mBirk[3];
+  double mEsum;
+
+  ClassDef(StCalorimeterHitCollection,0);
+
+};
+
+#endif
