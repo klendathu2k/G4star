d662ca67e1dc75e5d228c900e0cb2c36d1822cb1
klendathu2k <jwebb@bnl.gov>
2020-05-11 15:29:08 -0400

    Header file may be needed for TPC volume ID...

---
+++ StRoot/StGeant4Maker/AgMLTpcVolumeId.h
@@ -0,0 +1,32 @@
+#ifndef __AgMLTpcVolumeId_h__
+#define __AgMLTpcVolumeId_h__
+
+#include <StarVMC/StarAgmlLib/AgMLExtension.h>
+#include <StMessMgr.h>
+
+class AgMLTpcVolumeId : public AgMLVolumeId {
+public:
+  
+  AgMLTpcVolumeId();
+
+  int tpads[76];
+  int isdet[76];
+
+  virtual int id( int* numbv ) const { 
+
+    int tpgv = numbv[0] + 1; // tpc gas volume 1, 2
+    int tpss = numbv[1] + 1; // tpc super sector 1-12
+    int tpad = numbv[2] + 1; // tpc padrow
+
+    int sector = tpss + 12 * (tpgv-1); // sector 1-24
+
+    int det = isdet[tpad-1];
+    int pad = tpads[tpad-1];
+
+    return 100000*det + 100*sector + pad;
+
+  };
+};
+
+
+#endif
