bea347fdf46afcbd1bf93b84fea7abce8f7aa4c0
klendathu2k <jwebb@bnl.gov>
2020-06-19 12:47:13 -0400

    Add a verbose option which outputs vertex and track information.

--- macros/test_stress_acceptance.C
+++ macros/test_stress_acceptance.C
@@ -4,19 +4,32 @@
 #include <TVirtualMC.h>
 #include <StMessMgr.h>
 
-void test_stress_acceptance(int nevents=10, int ntracks=10,const char* particles="pi+,pi-,K+,K-,mu+,mu-,e+,e-,proton,antiproton,pi0,gamma") {
+void test_stress_acceptance(int nevents=10, int ntracks=10,const char* particles="pi+,pi-,K+,K-,mu+,mu-,e+,e-,proton,antiproton,pi0,gamma", const bool verbose=false ) {
 
   double ptmn =  0.100; 
   double ptmx = 10.000;
   double etamn= -2.0; 
   double etamx= +4.0;
 
-  // Configure G4 maker 
-  StMaker* _geant4 = chain->GetMaker("geant4");
+  StMaker*           _geant4 = chain->GetMaker("geant4star");
 
   // Run particle gun simulation
   for ( int i=0;i<nevents;i++ ) {
+
     particleGun(ntracks,particles,ptmn,ptmx,etamn,etamx);
+    if ( false == verbose ) continue;
+
+    // Get the particle stack for inspection
+    StMCParticleStack* _stack = (StMCParticleStack*)TVirtualMC::GetMC()->GetStack();
+
+    for ( auto vert : _stack->GetVertexTable() ) {
+      LOG_INFO << *vert << endm;
+    }
+
+    for ( auto part : _stack->GetParticleTable() ) {
+      LOG_INFO << *part << endm;
+    }
+
   }
 
 
