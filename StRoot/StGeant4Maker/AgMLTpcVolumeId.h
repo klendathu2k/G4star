5294ab4964ed0ac573c2c40fc338c370828c60f2
klendathu2k <jwebb@bnl.gov>
2020-06-25 16:33:23 -0400

    Fix off-by-one issue in volume numbering

--- StRoot/StGeant4Maker/AgMLTpcVolumeId.h
+++ StRoot/StGeant4Maker/AgMLTpcVolumeId.h
@@ -14,16 +14,18 @@ public:
 
   virtual int id( int* numbv ) const { 
 
-    int tpgv = numbv[0] + 1; // tpc gas volume 1, 2
-    int tpss = numbv[1] + 1; // tpc super sector 1-12
-    int tpad = numbv[2] + 1; // tpc padrow
+    int tpgv = numbv[0]; // tpc gas volume 1, 2
+    int tpss = numbv[1]; // tpc super sector 1-12
+    int tpad = numbv[2]; // tpc padrow
 
-    int sector = tpss + 12 * (tpgv-1); // sector 1-24
+    int sector = tpss + 12 * (tpgv-1); // sector 1-24    
 
     int det = isdet[tpad-1];
     int pad = tpads[tpad-1];
 
-    return 100000*det + 100*sector + pad;
+    int volumeid = 100000*det + 100*sector + pad;                
+
+    return volumeid;
 
   };
 };
