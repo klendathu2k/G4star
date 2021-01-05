451c038888e3ea386e29dfbf7c31687746bbab8b
klendathu2k <jwebb@bnl.gov>
2021-01-05 09:41:09 -0500

    This change should not be propagated back onto the main branch.  Removed only because it does not compile in eval area under root 6.16.00.

--- StRoot/StEpdUtil/StEpdUtilLinkDef.h
+++ StRoot/StEpdUtil/StEpdUtilLinkDef.h
@@ -4,10 +4,6 @@
 #pragma link off all classes;
 #pragma link off all functions;
 
-// Main PicoDst classes
-#pragma link C++ class StBbcGeom+;
 #pragma link C++ class StEpdGeom+;
-#pragma link C++ class StEpdEpInfo+;
-#pragma link C++ class StEpdEpFinder+;
 
 #endif
