8f873723b5e99adaa886794edd854d9537553257
klendathu2k <jwebb@bnl.gov>
2021-01-14 10:16:27 -0500

    Fill out the test suite for the VPD

--- tests/unit_test_vpd_hits.C
+++ tests/unit_test_vpd_hits.C
@@ -1,6 +1,15 @@
 #include "tests/unit_tests.h"
 #include <assert.h>
 
+
+//___________________________________________________________________
+std::string check_vpd_hit( std::string message, const g2t_vpd_hit_st* hit, std::function<std::string(const g2t_vpd_hit_st*)> f) {
+  std::string result = "\u001b[37m [" + message + "] " + (hit? f(hit):FAIL);
+  LOG_TEST << result << std::endl;
+  return result;
+};
+
+
 // TODO: Implement test of prompt-hits
 
 #ifndef __CINT__
@@ -79,11 +88,13 @@ void unit_test_vpd_hits( int longtest=0 ) {
   };
 
 
-
-  for ( double side = 1.0; side >= -1.0; side -= 2.0 ) 
+  for ( double side = 1.0; side >= -1.0; side -= 2.0 ) {
+    int detector=0; // detector copy number
   for ( auto point : pos ) {
 
 
+
+    detector++;
 
     LOG_TEST << "======================================================================================" << std::endl;
     LOG_TEST << "Target VPD detector at " << point.x << " " << point.y << " " << point.z << std::endl;
@@ -93,7 +104,7 @@ void unit_test_vpd_hits( int longtest=0 ) {
     double phi = p.Phi() * 180.0 / TMath::Pi();
     timer.Start();
     time( timer.CpuTime() );
-    throw_muon( eta, phi, 5.0 );
+    throw_muon( eta, phi, 0.35 );
     timer.Reset();
 
     auto* chain = StMaker::GetChain();
@@ -154,7 +165,7 @@ void unit_test_vpd_hits( int longtest=0 ) {
 	if ( t->eta ==-999 ) result = FAIL;
 	return result;
       });
-    check_track( Form("The track should have an eta=%f",_eta),     [=](const g2t_track_st* t){
+    check_track( Form("The track should have the correct  eta"),     [=](const g2t_track_st* t){
 	double delta = abs(t->eta-_eta);	
 	return abs(t->eta-_eta)<1E-5 ?PASS:FAIL;      
     });
@@ -167,7 +178,139 @@ void unit_test_vpd_hits( int longtest=0 ) {
     });    
 
 
-  };
+    for ( int i=0;i<hit_table->GetNRows();i++ ) {
+
+      auto hit = static_cast<const g2t_vpd_hit_st*>( hit_table->At(i) );
+      if ( 0==hit ) continue;     // skip null entries
+      if ( 1!=hit->track_p ) continue; // not interested in secondaries
+
+      LOG_TEST << "------------------------------------------------------------------" << std::endl;
+      LOG_TEST << GIVEN << "A hit on that track" << std::endl;
+
+
+      check_vpd_hit( "The hit should have a nonzero volume_id",                    hit,[=](const g2t_vpd_hit_st* h) {
+       	  std::string result = FAIL;
+          if ( h->volume_id > 0 ) result = PASS;
+     	  result = Form("id=%i vid=%i de=%f ds=%f ",h->id,h->volume_id,h->de,h->ds) + result;
+     	  return result;
+     	});
+
+      check_vpd_hit( "The hit should have an energy deposit > 0",                  hit,[=](const g2t_vpd_hit_st* h) {
+       	  std::string result = FAIL; // undetermined
+	  if       ( h->de > 0 ) result = PASS;
+	  return result;
+	});
+      check_vpd_hit( "The hit should have a step size > 0",                        hit,[=](const g2t_vpd_hit_st* h) {
+	  std::string result = FAIL;
+	  if ( h->ds > 0 ) result = PASS;
+	  return result;
+	});
+      check_vpd_hit( "The hit should have a path length > 0",                      hit,[=](const g2t_vpd_hit_st* h) {
+	  std::string result = FAIL;
+	  if ( h->s_track > 0 ) result = PASS;
+	  return result;
+	});
+      check_vpd_hit( "The hit should have time-of-flight > 0",                     hit,[=](const g2t_vpd_hit_st* h) {
+	  std::string result = FAIL;
+	  if ( h->tof > 0 ) result = PASS;
+	  result = Form(" tof=%f ",h->tof) + result;
+	  return result;
+	});
+      check_vpd_hit( "The hit should have a nonzero momentum",                     hit,[=](const g2t_vpd_hit_st* h) {
+	  std::string result = FAIL;
+	  if ( h->p[0] != 0 ) result = PASS;
+	  if ( h->p[1] != 0 ) result = PASS;
+	  if ( h->p[2] != 0 ) result = PASS;
+	  return result;
+	});
+      check_vpd_hit( "The hit should have a length > 0",                           hit,[=](const g2t_vpd_hit_st* h) {
+	  std::string result = FAIL;
+	  if ( h->s_track > 0 ) result = PASS;
+	  return result;
+	});
+      check_vpd_hit( "The track length and tof*c agree to w/in 0.15 mm ",          hit,[=](const g2t_vpd_hit_st* h) {
+	  // There should be some tolerance on this, b/c of roundoff error at each tracking step
+	  std::string result = FAIL;
+	  double c_tof = 2.99792458E10 /* cm/s */ * h->tof;
+	  double s_trk = h->s_track;
+	  double diff = abs(c_tof-s_trk);
+	  if ( diff < 0.015 ) result = PASS;
+	  result = Form("c_tof=%f cm  strack=%f cm diff=%f cm ",c_tof,s_trk,diff) + result;
+	  return result;
+	});
+      // check_vpd_hit( "The hit be at a radius > 207.8",                             hit,[=](const g2t_vpd_hit_st* h) {
+      // 	  std::string result = FAIL;
+      // 	  double R2 = h->x[0]*h->x[0] + h->x[1]*h->x[1];
+      // 	  if ( R2 > 207.8*207.8 ) result = PASS;
+      // 	  result = Form(" R=%f ",sqrt(R2)) + result;
+      // 	  return result;
+      // 	});
+      // check_vpd_hit( "The hit be at a radius < 221.0",                             hit,[=](const g2t_vpd_hit_st* h) {
+      // 	  std::string result = FAIL;
+      // 	  double R2 = h->x[0]*h->x[0] + h->x[1]*h->x[1];
+      // 	  if ( R2 < 221.0*221.0 ) result = PASS;
+      // 	  result = Form(" R=%f ",sqrt(R2)) + result;
+      // 	  return result;
+      // 	});
+      check_vpd_hit( "The track length at the hit should be >= radius at the hit", hit,[=](const g2t_vpd_hit_st* h) {
+	  std::string result = FAIL;
+	  double R2 = h->x[0]*h->x[0] + h->x[1]*h->x[1];
+	  double L2 = h->s_track * h->s_track;
+	  if ( R2 <= L2 ) result = PASS;
+	  result = Form(" R=%f L=%f",sqrt(R2),sqrt(L2)) + result;
+	  return result;
+	});
+      check_vpd_hit( "Track's momentum at hit should be < initial value",          hit,[=](const g2t_vpd_hit_st* h) {
+	  std::string result = FAIL;
+	  double px = h->p[0];
+	  double py = h->p[1];
+	  double pz = h->p[2];
+	  double p2 = px*px + py*py + pz*pz;
+	  if ( p2 < _pmom*_pmom ) result = PASS;
+	  return result;
+	});
+
+      // volume_id  =  1000*rileft + 100*innout + sector
+
+      int expected_rileft = (_eta>0)? 1 : 2;
+      check_vpd_hit( Form("VPD hit should be on side %i",expected_rileft),    hit,[=](const g2t_vpd_hit_st* h) {
+	  std::string result = FAIL;
+	  int rileft = h->volume_id / 1000;
+	  result = ( rileft==expected_rileft) ? PASS : FAIL;
+	  return result;
+	});
+
+      check_vpd_hit( Form("VPD hit should be on expected detector"),    hit,[=](const g2t_vpd_hit_st* h) {
+	  std::string result = FAIL;
+	  int det = h->volume_id %100;
+	  result = ( detector==det) ? PASS : FAIL;
+	  return result;
+	});
+
+      // Following tests are only for "primary hits"
+
+      if ( hit->s_track <= 500 ) continue;
+
+      check_vpd_hit( Form("VPD hit should be on side %i {s_track>500}",expected_rileft),    hit,[=](const g2t_vpd_hit_st* h) {
+	  std::string result = FAIL;
+	  int rileft = h->volume_id / 1000;
+	  result = ( rileft==expected_rileft) ? PASS : FAIL;
+	  return result;
+	});
+
+      check_vpd_hit( Form("VPD hit should be on expected detector {s_track>500}"),    hit,[=](const g2t_vpd_hit_st* h) {
+	  std::string result = FAIL;
+	  int det = h->volume_id %100;
+	  result = ( detector==det) ? PASS : FAIL;
+	  return result;
+	});
+
+
+
+    }    
+
+  };// loop over detectors
+  }
 
 
 };
